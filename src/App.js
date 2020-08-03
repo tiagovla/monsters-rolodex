5a80743ae83b2137440cf456f4ebe3cfba677f0b
Tiago Vilela <tiagovla@gmail.com>
2020-08-03 09:28:03 -0300

    final version

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component'
+
+import './App.css';
+
+
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+
+  render(){
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monter => monter.name.toLowerCase().includes(searchField.toLowerCase()))
+
+    return (
+    <div className='App'>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox
+        placeholder='search monster'
+        handleChange={this.handleChange}
+      />
+      <CardList monsters={filteredMonsters}/>
+    </div>    
+    );
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+}
+
+ 
+export default App;
